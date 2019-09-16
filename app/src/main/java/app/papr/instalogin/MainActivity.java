7faa5b4a7f9f5059f50947f844b295bf0e6026d8
Grahamj1 <jayygraham@gmail.com>
2019-09-15 18:35:39 -0700

    enabled multiDexFiles to allow the project to compile

--- app/src/main/java/app/papr/instalogin/MainActivity.java
+++ app/src/main/java/app/papr/instalogin/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }
 
     public void fun(View e){
